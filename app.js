6a163af5e5435260d3bb1f77d7399ba87c9aa377
yohannesjho <jhonians@outlook.com>
2024-11-07 14:48:21 +0300

    Add .env file

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 const methodOverride = require('method-override');
 const session = require('express-session');
-
-
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
